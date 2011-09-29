f55388c781f31a79c2d3f00ba2181209b5653476
mofaph <mofaph@gmail.com>
2011-09-29 21:24:00 +0800

    simpleshell.c -- 加入中断信号处理
    如果调用未经修改的版本，然后键入中断键（通常是 Delete 键或 Ctrl-C），
    则执行此程序的进程终止。产生这种后果的原因是，对于此信号（SIGINT）的
    系统默认动作是终止进程。该进程没有告诉系统内核对此信号作何种处理，
    所以系统按默认方式终止该进程。
    
    为使该进程捕捉到此信号，它需要调用 signal 函数，由其指定当产生 SIGINT
    信号时要调用的函数名。为此编写了名为 sig_int 的函数，当其被调用时，
    它只是打印一条消息，然后打印一个新提示符。
    
    在 Ubuntu Linux 10.04 上测试，只是打印完消息就退出了。

--- simpleshell.c
+++ simpleshell.c
@@ -1,12 +1,17 @@
 #include "apue.h"
 #include <sys/wait.h>
 
+static void sig_int(int);       /* our signal-catching function */
+
 int
 main(void)
 {
   char buf[MAXLINE];            /* from apue.h */
   pid_t pid;
   int status;
+
+  if (signal(SIGINT, sig_int) == SIG_ERR)
+    err_sys("signal error");
 
   printf("%% ");                /* print prompt (printf requires %% to print %) */
   while (fgets(buf, MAXLINE, stdin) != NULL) {
@@ -27,4 +32,10 @@ main(void)
     printf("%% ");
   }
   exit(0);
+}
+
+void
+sig_int(int signo)
+{
+  printf("interrupt\n%% ");
 }
