ab944361a27f7b6c47e102c656d2c565966c68f6
mofaph <mofaph@gmail.com>
2011-11-04 16:08:57 +0800

    程序清单 4-2 access 函数实例
    open() 打开一个文件时，内核以进程的有效用户 ID 和有效组 ID 为基础
    执行其访问权限测试。
    
    access() 是按实际用户 ID 和实际组 ID 进行访问权限测试的。
    
    有效用户 ID 是相对进程而言的。对于进程来说，它的有效用户 ID 是执行
    这个进程的用户的 ID。
    
    实际用户 ID 是相对文件而言的。对于文件来说，有效用户 ID 指明了拥有
    这个文件的用户的 ID。

---
+++ figure4-2.c
@@ -0,0 +1,22 @@
+/* 程序清单 4-2 access 函数实例 */
+
+#include "apue.h"
+#include <fcntl.h>
+
+int
+main(int argc, char *argv[])
+{
+  if (argc != 2)
+    err_quit("usage: a.out <pathname>");
+  if (access(argv[1], R_OK) < 0)
+    err_ret("access error for %s", argv[1]);
+  else
+    printf("read access OK\n");
+  if (open(argv[1], O_RDONLY) < 0)
+    err_ret("open error for %s", argv[1]);
+  else
+    printf("open for reading OK\n");
+  exit(0);
+}
+
+/* figure4-2.c ends here */
