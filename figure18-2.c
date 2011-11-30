8c15b0994eeadeee046ef465251dd97b60a885cb
mofaph <mofaph@gmail.com>
2011-11-30 15:06:33 +0800

    程序清单 18-2 tcgetattr 和 tcsetattr 实例
    编译方法：
        $ gcc err.c figure18-2.c

---
+++ figure18-2.c
@@ -0,0 +1,41 @@
+/*
+ * 程序清单 18-2 tcgetattr 和 tcsetattr 实例
+ */
+
+#include "apue.h"
+#include <termios.h>
+
+int
+main(void)
+{
+  struct termios term;
+
+  if (tcgetattr(STDIN_FILENO, &term) < 0)
+    err_sys("tcgetattr error");
+
+  switch (term.c_cflag & CSIZE) {
+  case CS5:
+    printf("5 bits/byte\n");
+    break;
+  case CS6:
+    printf("6 bits/byte\n");
+    break;
+  case CS7:
+    printf("7 bits/byte\n");
+    break;
+  case CS8:
+    printf("8 bits/byte\n");
+    break;
+  default:
+    printf("unknown bits/byte\n");
+  }
+
+  term.c_cflag &= ~CSIZE;       /* zero out the bits */
+  term.c_cflag |= CS8;          /* set 8 bits/byte */
+  if (tcsetattr(STDIN_FILENO, TCSANOW, &term) < 0)
+    err_sys("tcsetattr error");
+
+  exit(0);
+}
+
+/* figure18-2.c ends here */
