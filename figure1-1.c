8b458dde4462f201c37e63c78433558da2db4fdf
mofaph <mofaph@gmail.com>
2011-11-30 22:08:34 +0800

    figure1-1.c: 缩进风格改为 GNU 风格

--- figure1-1.c
+++ figure1-1.c
@@ -10,19 +10,19 @@
 int
 main(int argc, char *argv[])
 {
-    DIR           *dp;
-    struct dirent *dirp;
+  DIR           *dp;
+  struct dirent *dirp;
 
-    if (argc != 2)
-        err_quit("usage: ls directory_name");
+  if (argc != 2)
+    err_quit("usage: ls directory_name");
 
-    if ((dp = opendir(argv[1])) == NULL)
-        err_sys("can't open %s", argv[1]);
-    while ((dirp = readdir(dp)) != NULL)
-        printf("%s\n", dirp->d_name);
+  if ((dp = opendir(argv[1])) == NULL)
+    err_sys("can't open %s", argv[1]);
+  while ((dirp = readdir(dp)) != NULL)
+    printf("%s\n", dirp->d_name);
 
-    closedir(dp);
-    exit(0);
+  closedir(dp);
+  exit(0);
 }
 
 /* figure1-1.c ends here */
