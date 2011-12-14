a3b0c41526f674b407913ed616bee5117f9a6f87
mofaph <mofaph@gmail.com>
2011-12-14 09:09:41 +0800

    程序清单 8-6 具有竞争条件的程序
    编译方法：
        $ gcc err.c figure8-6.c
    
    当多个进程都企图对共享数据进行某种处理，而最后的结果又取决于进程运
    行的顺序时，则我们认为这发生了竞争条件（race condition）。
    
    为了避免竞争条件和轮询，在多个进程之间需要有某种形式的信号发送和接收
    的方法。
    
    在程序中将标准输出设置为不带缓冲的，于是每个字符输出都需要调用一次
    write()。本例的目的是使内核尽可能地在两个进程之间进行多次切换，以便
    演示竞争条件。

---
+++ figure8-6.c
@@ -0,0 +1,35 @@
+/*
+ * 程序清单 8-6 具有竞争条件的程序
+ */
+
+#include "apue.h"
+
+static void charatatime(char *);
+
+int
+main(void)
+{
+  pid_t pid;
+
+  if ((pid = fork()) < 0) {
+    err_sys("fork error");
+  } else if (pid == 0) {
+    charatatime("output from child\n");
+  } else {
+    charatatime("output from parent\n");
+  }
+  exit(0);
+}
+
+static void
+charatatime(char *str)
+{
+  char  *ptr;
+  int   c;
+
+  setbuf(stdout, NULL);         /* set unbuffered */
+  for (ptr = str; (c = *ptr++) != 0; )
+    putc(c, stdout);
+}
+
+/* figure8-6.c ends here */
