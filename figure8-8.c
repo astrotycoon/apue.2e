3790bb7044e74b18af65e4df3178d0df27ede027
mofaph <mofaph@gmail.com>
2011-12-15 19:15:36 +0800

    程序清单 8-8 exec 函数实例
    编译方法：
        $ gcc -Wall err.c figure8-8.c

---
+++ figure8-8.c
@@ -0,0 +1,36 @@
+/*
+ * 程序清单 8-8 exec 函数实例
+ */
+
+#include "apue.h"
+#include <sys/wait.h>
+
+char *env_init[] = { "USER=unknown", "PATH=/tmp", NULL };
+
+int
+main(void)
+{
+  pid_t pid;
+
+  if ((pid = fork()) < 0) {
+    err_sys("fork error");
+  } else if (pid == 0) {        /* specify pathname, specify environment */
+    if (execle("/home/mofaph/bin/echoall", "echoall", "myarg1",
+               "MY ARG2", (char *)0, env_init) < 0)
+      err_sys("execle error");
+  }
+
+  if (waitpid(pid, NULL, 0) < 0)
+    err_sys("wait error");
+
+  if ((pid = fork()) < 0) {
+    err_sys("fork error");
+  } else if (pid == 0) {        /* specify pathname, specify environment */
+    if (execlp("echoall", "echoall", "only 1 arg", (char *)0) < 0)
+      err_sys("execlp error");
+  }
+
+  exit(0);
+}
+
+/* figure8-8.c ends here */
