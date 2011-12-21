42794d68d2fcd9f3847fce476ef5cdf7bd9deb9d
mofaph <mofaph@gmail.com>
2011-12-21 21:44:55 +0800

    程序清单 10-1 捕捉 SIGUSR1 和 SIGUSR2 的简单程序
    编译方法：
        $ gcc err.c figure10-1.c
    
    pause(): 使调用进程在接到一个信号前挂起
    
    注意：在 UNIX 中，杀死（kill）这个术语是不恰当的。kill(1) 命令和
    kill(2) 函数只是将一个信号送给一个进程或进程组。信号是否终止进程则
    取决于信号的类型，以及进程是否安排了捕捉该信号。

---
+++ figure10-1.c
@@ -0,0 +1,31 @@
+/*
+ * 程序清单 10-1 捕捉 SIGUSR1 和 SIGUSR2 的简单程序
+ */
+
+#include "apue.h"
+
+static void sig_usr(int);       /* one handler for both signals */
+
+int
+main(void)
+{
+  if (signal(SIGUSR1, sig_usr) == SIG_ERR)
+    err_sys("can't catch SIGUSR1");
+  if (signal(SIGUSR2, sig_usr) == SIG_ERR)
+    err_sys("can't catch SIGUSR2");
+  for ( ; ; )
+    pause();
+}
+
+static void
+sig_usr(int signo)              /* argument is signal number */
+{
+  if (signo == SIGUSR1)
+    printf("received SIGUSR1\n");
+  else if (signo == SIGUSR2)
+    printf("received SIGUSR2\n");
+  else
+    err_dump("received signal %d\n", signo);
+}
+
+/* figure10-1.c ends here */
